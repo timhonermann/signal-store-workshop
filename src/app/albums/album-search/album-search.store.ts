2656a6aca3c638b88335f866951a099068afa6b1
markostanimirovic <markostanimirovic95@gmail.com>
2024-02-21 15:31:56 +0100

    07-global-state

--- src/app/albums/album-search/album-search.store.ts
+++ src/app/albums/album-search/album-search.store.ts
@@ -1,82 +1,50 @@
 import { computed, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { exhaustMap, filter, pipe, tap } from 'rxjs';
+import { filter, pipe, tap } from 'rxjs';
 import {
-  patchState,
   signalStore,
   withComputed,
   withHooks,
   withMethods,
 } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { setAllEntities, withEntities } from '@ngrx/signals/entities';
-import { tapResponse } from '@ngrx/operators';
 import { toSortOrder } from '@/shared/models/sort-order.model';
-import {
-  setError,
-  setFulfilled,
-  setPending,
-  withRequestStatus,
-} from '@/shared/state/request-status.feature';
 import { withQueryParams } from '@/shared/state/route/query-params.feature';
-import { Album, searchAlbums, sortAlbums } from '@/albums/album.model';
-import { AlbumsService } from '@/albums/albums.service';
+import { searchAlbums, sortAlbums } from '@/albums/album.model';
+import { AlbumsStore } from '@/albums/albums.store';
 
 export const AlbumSearchStore = signalStore(
-  withEntities<Album>(),
   withQueryParams({
     query: (param) => param ?? '',
     order: toSortOrder,
   }),
-  withRequestStatus(),
-  withComputed(({ entities, query, order, isPending }) => {
+  withComputed(({ query, order }, albumsStore = inject(AlbumsStore)) => {
     const filteredAlbums = computed(() => {
-      const searchedAlbums = searchAlbums(entities(), query());
+      const searchedAlbums = searchAlbums(albumsStore.entities(), query());
       return sortAlbums(searchedAlbums, order());
     });
 
     return {
       filteredAlbums,
-      showProgress: isPending,
-      showSpinner: computed(() => isPending() && entities().length === 0),
+      showProgress: albumsStore.isPending,
+      showSpinner: computed(
+        () => albumsStore.isPending() && albumsStore.entities().length === 0,
+      ),
       totalAlbums: computed(() => filteredAlbums().length),
     };
   }),
-  withMethods(
-    (
-      store,
-      albumsService = inject(AlbumsService),
-      snackBar = inject(MatSnackBar),
-    ) => ({
-      loadAllAlbums: rxMethod<void>(
-        pipe(
-          tap(() => patchState(store, setPending())),
-          exhaustMap(() => {
-            return albumsService.getAll().pipe(
-              tapResponse({
-                next: (albums) => {
-                  patchState(store, setAllEntities(albums), setFulfilled());
-                },
-                error: (error: { message: string }) => {
-                  patchState(store, setError(error.message));
-                },
-              }),
-            );
-          }),
-        ),
+  withMethods((_, snackBar = inject(MatSnackBar)) => ({
+    notifyOnError: rxMethod<string | null>(
+      pipe(
+        filter(Boolean),
+        tap((error) => snackBar.open(error, 'Close', { duration: 5_000 })),
       ),
-      notifyOnError: rxMethod<string | null>(
-        pipe(
-          filter(Boolean),
-          tap((error) => snackBar.open(error, 'Close', { duration: 5_000 })),
-        ),
-      ),
-    }),
-  ),
+    ),
+  })),
   withHooks({
-    onInit(store) {
-      store.loadAllAlbums();
-      store.notifyOnError(store.error);
+    onInit({ notifyOnError }, albumsStore = inject(AlbumsStore)) {
+      albumsStore.loadAllAlbums();
+      notifyOnError(albumsStore.error);
     },
   }),
 );
